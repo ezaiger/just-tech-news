31f6ca034636cc61a2ac203247784c75baa28652
EZaiger <elisabeth.zaiger@oit.edu>
2022-08-03 17:16:11 -0700

    updated comment routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
-const { User, Post, Vote } = require("../../models");
+const { User, Post, Comment, Vote } = require("../../models");
 
 // GET /api/users
 router.get("/", (req, res) => {
-  // Access our User mdoel and run .findAll() method
+  // Access our User model and run .findAll() method
   User.findAll({
     attributes: { exclude: ["password"] },
   })
@@ -27,12 +27,20 @@ router.get("/:id", (req, res) => {
         attributes: ["id", "title", "post_url", "created_at"],
       },
       {
+        model: Comment,
+        attributes: ["id", "comment_text", "created_at"],
+        include: {
+          model: Post,
+          attributes: ['title']
+        }
+      },
+      {
         model: Post,
         attributes: ["title"],
         through: Vote,
         as: "voted_posts",
-      },
-    ],
+      }
+    ]
   })
     .then((dbUserData) => {
       if (!dbUserData) {
